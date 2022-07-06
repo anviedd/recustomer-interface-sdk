732af35f181cb8502ad0aec6abe3ffa9e0a92599
ju12yw <76781368+ju12yw@users.noreply.github.com>
2022-07-06 18:57:57 +0900

    feat : Add update(put request) to order resouce

--- ec_cart/resources/Order.py
+++ ec_cart/resources/Order.py
@@ -11,3 +11,9 @@ class Order(ActiveResource):
 
     def delete(self, id_=None, **kwargs):
         raise exceptions.MethodNotAllowedError
+
+    def update(self, id_=None, **kwargs):
+        if id_:
+            kwargs['id'] = str(id_)
+
+        return self._put(**kwargs)
