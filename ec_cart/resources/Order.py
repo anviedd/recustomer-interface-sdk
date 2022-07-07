edda2cb5a3b91dec0ca6a0d2fe1754567196e1b3
ju12yw <76781368+ju12yw@users.noreply.github.com>
2022-07-07 13:01:53 +0900

    feat : add update method in Order resources

--- ec_cart/resources/Order.py
+++ ec_cart/resources/Order.py
@@ -9,11 +9,11 @@ class Order(ActiveResource):
     class Meta:
         model = OrderModel
 
-    def delete(self, id_=None, **kwargs):
-        raise exceptions.MethodNotAllowedError
-
     def update(self, id_=None, **kwargs):
         if id_:
             kwargs['id'] = str(id_)
 
         return self._put(**kwargs)
+
+    def delete(self, id_=None, **kwargs):
+        raise exceptions.MethodNotAllowedError
