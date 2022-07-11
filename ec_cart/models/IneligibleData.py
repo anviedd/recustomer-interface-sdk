c83ee03b48fc4ee541d54ccc8afa231a6d681b9c
ju12yw <76781368+ju12yw@users.noreply.github.com>
2022-07-11 12:01:33 +0900

    fix : ineligible data models fix

--- ec_cart/models/IneligibleData.py
+++ ec_cart/models/IneligibleData.py
@@ -1,7 +1,50 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Union, List
 
 from pydantic import BaseModel
 
 
+class CollectionsNode(BaseModel):
+    id: int = None
+    title: str = None
+
+
+class CollectionsEdges(BaseModel):
+    node: str = None
+
+
+class Collections(BaseModel):
+    edges: str = None
+
+
+class OrdersNode(BaseModel):
+    tags: List[str] = None
+
+
+class OrdersEdges(BaseModel):
+    node: OrdersNode = None
+
+
+class Orders(BaseModel):
+    orders: List[Dict[str, Union[int, str, float]]]
+
+
+class PriceRulesNode(BaseModel):
+    title: str = None
+
+
+class PriceRulesEdges(BaseModel):
+    node: PriceRulesNode = None
+
+
+class PriceRules(BaseModel):
+    edges: List[Union[PriceRulesEdges, None]] = None
+
+
+class IneligibleData(BaseModel):
+    priceRules: Optional[PriceRules] = None
+    orders: Optional[Orders] = None
+    collections: Optional[Collections] = None
+
+
 class IneligibleDataModel(BaseModel):
-    data: Optional[Dict[str, Optional[str]]] = None
+    data: IneligibleData = None
