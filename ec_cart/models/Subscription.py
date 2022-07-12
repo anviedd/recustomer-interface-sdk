586a10a2cc17897a6bcb4a0a30a3d133ba2bd650
ju12yw <76781368+ju12yw@users.noreply.github.com>
2022-07-12 10:52:35 +0900

    fix : add [status_code] to Subscription's model

--- ec_cart/models/Subscription.py
+++ ec_cart/models/Subscription.py
@@ -6,7 +6,9 @@ class SubscriptionCreateModel(BaseModel):
     redirect_url: str = None
     charge_id: str = None
     line_item_id: str = None
+    status_code: int = None
 
 
 class SubscriptionCancelModel(BaseModel):
     result: bool = False
+    status_code: int = None
