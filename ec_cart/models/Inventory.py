74e4a713bfa0836d39b9a7924ef4484d71419661
ju12yw <76781368+ju12yw@users.noreply.github.com>
2022-07-27 11:26:36 +0900

    fix : fix model of inventory get request

--- ec_cart/models/Inventory.py
+++ ec_cart/models/Inventory.py
@@ -6,3 +6,21 @@ class InventoryModel(BaseModel):
     available: float = None
     inventory_item_id: int = None
     location_id: int = None
+
+
+class ArticleObject(BaseModel):
+    id: int = None
+    sku: str = None
+    name: str = None
+
+
+class WarehouseObject(BaseModel):
+    id: int = None
+    name: str = None
+
+
+class InventoryGetModel(BaseModel):
+    id: int = None
+    free: int = None
+    article: ArticleObject = None
+    warehouse: WarehouseObject = None
--- ec_cart/resources/Inventory.py
+++ ec_cart/resources/Inventory.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from ec_cart import exceptions
 from ec_cart.base import ActiveResource
-from ec_cart.models.Inventory import InventoryModel
+from ec_cart.models.Inventory import InventoryModel, InventoryGetModel
 
 
 class Inventory(ActiveResource):
@@ -13,6 +13,10 @@ class Inventory(ActiveResource):
 
     def create(self, **kwargs):
         return self._post(**kwargs)
+
+    def find(self, **kwargs):
+        self.Meta.model = InventoryGetModel
+        return self._get(**kwargs)
 
     def delete(self, id_=None, **kwargs):
         raise exceptions.MethodNotAllowedError
